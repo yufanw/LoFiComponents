dca342f86c9e2039126d5dec107f009b4bcffcf0
Yufan Wang <yufanwangcn@hotmail.com>
2018-07-31 16:29:50 -0700

    checkbox remove halfcircle

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -8,17 +8,14 @@ export default class Checkbox extends Component {
       <div className="checkbox-comp">
         <div className="checkbox-wrapper">
           <label className="container">
-            <span className="half-circle"></span>
             <input type="radio" name="radio"/>
             <span className="checkmark red"></span>
           </label>
           <label className="container">
-            <span className="half-circle"></span>
             <input type="radio" name="radio"/>
             <span className="checkmark yellow"></span>
           </label>
           <label className="container">
-            <span className="half-circle"></span>
             <input type="radio" name="radio"/>
             <span className="checkmark green"></span>
           </label>
